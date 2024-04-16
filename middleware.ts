3de23cf59733581eb4c6b940e7b7e5588c3ece5e
Alex <alexandr.mameev@theloop.pt>
2024-04-16 22:40:50 +0200

    Fix

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+ 
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk'],
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
  
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
